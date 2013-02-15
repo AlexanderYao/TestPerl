5f82e8a37273e8949afc20e60524ab71b411e12d
AlexanderYao <dwy4670@gmail.com>
2013-02-15 00:03:10 -0800

    chapter5.pl
    输入输出

---
+++ chapter5.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use 5.010;
+
+while (<STDIN>) {
+  print "I saw $_";
+}
+my $line;
+if (@ARGV == 0) {
+  @ARGV = qw(template.pl test.pl);
+}
+while (<>) {
+  chomp;
+  $line++;
+  say "$line\t$_";
+}
