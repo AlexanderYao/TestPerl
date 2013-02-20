adbe3c62f01f21e5086efc22bd5ef18f7292ab22
AlexanderYao <dwy4670@gmail.com>
2013-02-20 22:50:04 +0800

    完成了第6章的前2个习题

--- chapter6.pl
+++ chapter6.pl
@@ -4,12 +4,32 @@ use warnings;
 use 5.010;
 
 my %hash = (
-    "fred" => 1,
-    "dino" => 2,
-    "barney" => 3,
-    "betty" => 4,
+    "alex" => 1,
+    "bash" => 2,
+    "clear" => 3,
+    "dir" => 4,
 );
-my @array = %hash;
-my %reverse_hash = reverse %hash;
-say "@array";
-say %reverse_hash;
+&print_hash(%hash);
+chomp(my $name = <STDIN>);
+say $hash{$name};
+
+chomp(my @names = <STDIN>);
+my %hash_names;
+foreach (@names){
+    $hash_names{$_} += 1;
+}
+&print_hash(%hash_names);
+
+sub print_hash {
+    my(%hash) = @_;
+#while(my ($key, $value)=each %hash){
+#        say "$key => $value";
+#    }
+    foreach my $key (sort keys %hash) {
+        say "$key => $hash{$key}";
+    }
+}
+
+#foreach my $key (sort keys %ENV){
+#    say "$key => $ENV{$key}";
+#}
