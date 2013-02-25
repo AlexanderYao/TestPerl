f3de57f0d7d7b0ce2e45632a8ce2c8171ad45b17
AlexanderYao <dwy4670@gmail.com>
2013-02-25 22:00:41 +0800

    完成第8章

--- chapter8.pl
+++ chapter8.pl
@@ -3,4 +3,38 @@ use strict;
 use warnings;
 use 5.010;
 
-print "Hello, World...\n";
+while (<>){
+	&_s($_);
+}
+
+sub _a {
+	if (/\b\w*a\b/) {
+		print "$`<$&>$'";
+	}
+}
+
+sub _1 {
+	if (/(\b\w*a\b)/) {
+		say "$1 contains '$1'";
+	}
+}
+
+sub _label {
+	if (/(?<label>\b\w*a\b)/) {
+		say "$+{label} contains '$+{label}'";
+	}
+}
+
+sub _a5 {
+	if (/\b\w*a\b/) {
+		print "$`<$&>$'";
+		my $part = substr($',0,5);
+		say "5 char: $part";
+	}
+}
+
+sub _s {
+	if (/\h+$/) {
+		print "$`<$&>$'";
+	}
+}
